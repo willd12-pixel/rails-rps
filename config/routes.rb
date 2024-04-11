7822b2e9aa35cd2c77637984f80017ebb8f2482b
willd12-pixel <143421896+willd12-pixel@users.noreply.github.com>
2024-04-11 14:01:04 +0000

    zebra

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-
-end
+  get("/rock", { :controller => "zebra", :action => "sample1"})
+  get("/paper", { :controller => "zebra", :action => "sample2"})
+  get("/scissors", { :controller => "zebra", :action => "sample3"})
+  get("/", { :controller => "zebra", :action => "rules"}) 
+end 
